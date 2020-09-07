7198b490790163ee219ffccf4da31bacaa3eb082
Chenghao Mou <mouchenghao@gmail.com>
2020-09-06 21:32:24 -0700

    add classification benchmarks

---
+++ benchmarks/classification.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Update       : 2020-09-06 21:05:58
+# @Author       : Chenghao Mou (mouchenghao@gmail.com)
+
+"""Classification benchmarks."""
+
+from loguru import logger
+from nlp import load_dataset
+from sklearn.metrics import classification_report
+
+from old_fashioned_nlp.classification import TfidfLinearSVCClassifier
+
+
+def benchmark_classification():
+
+    model = TfidfLinearSVCClassifier()
+
+    glue_cola = load_dataset("glue", "cola")
+    model.fit(glue_cola["train"]["sentence"], glue_cola["train"]["label"])
+    logger.info(
+        "GLUE/COLA\n"
+        + classification_report(
+            glue_cola["validation"]["label"],
+            model.predict(glue_cola["validation"]["sentence"]),
+        )
+    )
+
+    glue_sst = load_dataset("glue", "sst2")
+    model.fit(glue_sst["train"]["sentence"], glue_sst["train"]["label"])
+    logger.info(
+        "GLUE/COLA\n"
+        + classification_report(
+            glue_sst["validation"]["label"],
+            model.predict(glue_sst["validation"]["sentence"]),
+        )
+    )
+
+    yelp = load_dataset("yelp_polarity")
+    model.fit(yelp["train"]["text"], yelp["train"]["label"])
+    logger.info(
+        "Yelp\n"
+        + classification_report(
+            yelp["test"]["label"], model.predict(yelp["test"]["text"])
+        )
+    )
+
+    ag_news = load_dataset("ag_news")
+    model.fit(ag_news["train"]["text"], ag_news["train"]["label"])
+    logger.info(
+        "AG News\n"
+        + classification_report(
+            ag_news["test"]["label"], model.predict(ag_news["test"]["text"])
+        )
+    )
+
+    allocine = load_dataset("allocine")
+    model.fit(allocine["train"]["review"], allocine["train"]["label"])
+    logger.info(
+        "allocine\n"
+        + classification_report(
+            allocine["test"]["label"], model.predict(allocine["test"]["review"])
+        )
+    )
+
+    # anli = load_dataset('anli')
+    # model.fit(anli["train"]["premise"] + " " + anli["train"]["hypothesis"], anli["train"]["label"])
+    # logger.info('allocine\n' + classification_report(anli["test"]["label"], model.predict(anli["test"]["premise"] + " " + anli["test"]["hypothesis"])))
+
+    yelp = load_dataset("yelp_polarity")
+    model.fit(yelp["train"]["text"], yelp["train"]["label"])
+    logger.info(
+        "Yelp\n"
+        + classification_report(
+            yelp["test"]["label"], model.predict(yelp["test"]["text"])
+        )
+    )
+
+
+if __name__ == "__main__":
+
+    benchmark_classification()
